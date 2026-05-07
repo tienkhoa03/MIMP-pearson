a0b6286cd36eb2330c50cb55c1f7c3859e2048a6
Nguyen Tien Khoa <khoa.nguyentien@hcmut.edu.vn>
2026-05-07 07:37:16 +0700

    Update Airquality and ICU experiment parameters and output paths

--- continuous/run_airquality_experiments.ps1
+++ continuous/run_airquality_experiments.ps1
@@ -22,7 +22,7 @@ try {
     $window = 2
     $bases = @("SAGE++DAMC")
     $evalRatios = @(0.8)
-    $deltas = @(0.3, 0.4, 0.5)
+    $deltas = @(0.6, 0.7, 0.8)
     $streamRatios = @(0.001, 0.01, 0.1, 1.0)
     $increModes = @("alone", "data", "transfer", "data+state+transfer")
 
